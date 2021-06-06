a50ae5594b1952b7ec2c4ffddcd88f54970b6ed1
pauloddr <pauloddr@users.noreply.github.com>
2021-06-06 13:01:08 -0400

    chore: tweaks

--- src/constants.js
+++ src/constants.js
@@ -4,11 +4,18 @@
 
 /**
  * The full list of colors.
- * The resulting class name in HTML output will be prepended with "wm".
  */
 export const COLORS = 'white|silver|gray|red|maroon|yellow|olive|lime|green|blue|navy|cyan|teal|purple|magenta|gold|orange|darkorange|orangered|brown|dimgray|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15'
 
 /**
-  * The main Regular Expression to perform HTML tag replacement.
+ * The template for the resulting replacement.
+ * All class names in outputted HTML will be prefixed.
+ * You can change the prefix here. If you do, remember to
+ * also adjust the CSS file as well.
+ */
+export const HTML_REPLACEMENT_TEMPLATE = '<span class="wm$2">$3</span>'
+
+/**
+  * The main Regular Expression for performing replacements.
   */
 export const COLOR_RX = new RegExp('({((?:' + COLORS + '))}((?:(?!{(' + COLORS + '|/)}).)*)({/})*)', 'gim')
--- src/html.js
+++ src/html.js
@@ -2,13 +2,13 @@
 
 'use strict'
 
-import { COLOR_RX } from './constants.js'
+import { COLOR_RX, HTML_REPLACEMENT_TEMPLATE } from './constants.js'
 import escapeHtml from 'escape-html'
 
 /**
  * Convert color tags into HTML tags.
  * @param {string} text
- * @param {boolean} escape - when `false`,  will not escape HTML before parsing. Defaults to `true`.
+ * @param {boolean} escape - when `false`, will not escape HTML before parsing. Defaults to `true`.
  * @return {string}
  */
 export function html (text, escape = true) {
@@ -18,5 +18,5 @@ export function html (text, escape = true) {
   } else {
     escapedText = text
   }
-  return escapedText.replace(COLOR_RX, '<span class="wm$2">$3</span>')
+  return escapedText.replace(COLOR_RX, HTML_REPLACEMENT_TEMPLATE)
 }
